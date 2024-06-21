001621ff12c87cd587e544232a580664b7fa34fc
flashgun <flashgun@xboxstat.ru>
2024-06-21 16:00:10 +0000

    initial: reading catalogs

---
+++ api/getjson.php
@@ -0,0 +1,36 @@
+<?php
+
+header('Content-type: application/json');
+header("Cache-control: private");
+header("Cache-control: max-age=1");
+
+foreach ($_GET as $k => $v)
+	if(preg_match('/[^0-9a-z-]/', $k) || preg_match('/[^0-9a-zA-Z,-\/]/', $v))
+		die("Oops");
+
+$db = pg_connect("port=6432 dbname=global user=readonly password=masha27uk")	# , PGSQL_CONNECT_FORCE_NEW)
+	or die("could not connect to DB");
+
+$rep = array();
+
+$_GET['f']();
+
+echo json_encode($rep, JSON_UNESCAPED_UNICODE);
+
+# functions goes here
+
+function getcatalogs()	{
+
+	global $db, $rep;
+
+	static $cats = array(
+		"country" => "countryid as id,country as name,name as desc from stattotals1 join countries using(countryid)",
+		"lang" => "langid as id,lang as name,name as desc from stattotals1 join languages using(langid)",
+		"genre" => "genreid as id,genre as name,'' as desc from genres join gamegenres using(genreid) join stattotals1 using(titleid)",
+		"game" => "titleid as id,name,'' as desc from games join stattotals1 using(titleid)"
+	);
+
+	foreach ($cats as $k => $v)
+		$rep[$k] = pg_fetch_all(pg_query("select distinct $v"));
+
+}
--- scripts.js
+++ scripts.js
@@ -1,5 +1,31 @@
+var blocks = {
+	info: {
+	},
+	country: {
+	},
+	lang: {
+	},
+	genre: {
+	},
+	game: {
+	},
+};
+
 function main()	{
 
 	console.log('hi');
+	read_catalogs();
+
+}
+
+function read_catalogs()	{
+
+	fetch("api/getjson.php?f=getcatalogs")
+	.then( res => res.json )
+	.then( res => {
+
+		console.log(res);
+
+	});
 
 }
