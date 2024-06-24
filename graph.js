ab22361dcf2a6f78bf963f051943cbca1f8b19f4
flashgun <flashgun@xboxstat.ru>
2024-06-24 10:05:18 +0000

    added graph.js

---
+++ graph.js
@@ -0,0 +1,24 @@
+// data[id]: {
+//  	utime:
+//  	time:
+//  	players:
+//  	secs:
+//  	refpl:
+//  	refsecs:
+//  	val:
+//  	valabs:
+//  	valper:
+// }
+
+var graph = {
+	data: {},
+};
+
+function read_graph()	{
+
+}
+
+function draw_graph()	{
+
+}
+
